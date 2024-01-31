065369d79ddeffcf5cd1c0961cdd151821f59587
mrtstg <russiankurwacsgo@gmail.com>
2024-01-31 20:30:11 +0300

    changed indentation for haskell

--- .config/nvim/lua/augroups.lua
+++ .config/nvim/lua/augroups.lua
@@ -1,4 +1,5 @@
 black_on_save_group = vim.api.nvim_create_augroup("black_on_save_group", { clear = true })
+haskell_indent_group = vim.api.nvim_create_augroup("haskell_indent_group", {})
 
 local function launch_black()
     vim.api.nvim_command ':!black --quiet %'
@@ -10,6 +11,14 @@ vim.api.nvim_create_autocmd(
         pattern = '*.py',
         callback = launch_black,
         group = black_on_save_group
+    }
+)
+
+vim.api.nvim_create_autocmd(
+    { "BufEnter", "BufWinEnter" },
+    {
+        pattern = '*.hs',
+        command = "setlocal ts=2 sw=2"
     }
 )
 
