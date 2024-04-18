07376c800f368e0a4212f8ce800b0fc3567d732f
mrtstg <russiankurwacsgo@gmail.com>
2024-04-18 21:23:53 +0300

    feat: added indent of two spaces for haskell and svelte

--- .config/nvim/lua/augroups.lua
+++ .config/nvim/lua/augroups.lua
@@ -22,5 +22,13 @@ vim.api.nvim_create_autocmd(
     }
 )
 
+vim.api.nvim_create_autocmd(
+    { "BufEnter", "BufWinEnter" },
+    {
+        pattern = '*.{ts,svelte,js}',
+        command = "setlocal ts=2 sw=2"
+    }
+)
+
 -- Show diagnostics under the cursor when holding position
 vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
