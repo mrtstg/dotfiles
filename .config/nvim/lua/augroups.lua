60c2fcdff0915c4887a7c3f0f395d54cd047ff1f
mrtstg <mrtstgdev@gmail.com>
2025-08-26 09:25:14 +0300

    feat: hidden default error windows due to trouble.nvim usage

--- .config/nvim/lua/augroups.lua
+++ .config/nvim/lua/augroups.lua
@@ -31,4 +31,4 @@ vim.api.nvim_create_autocmd(
 )
 
 -- Show diagnostics under the cursor when holding position
-vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
+-- vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
